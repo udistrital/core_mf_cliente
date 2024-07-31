be8911aebc91b65b8be713702a80c50f563c01fe
diegobauto <diegobauto00@gmail.com>
2024-07-30 21:38:37 -0500

    feat: Actualización variables de entorno

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,9 +1,12 @@
 export const environment = {
   production: true,
   assets: 'https://pruebasassets.portaloas.udistrital.edu.co/',
-  CONFIGURACION_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
-  NOTIFICACION_SERVICE: 'wss://pruebasapi.portaloas.udistrital.edu.co:8116/ws',
+  CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
+
+  // Notificaciones
+  NOTIFICACION_MID_WS: "ws://autenticacion.portaloas.udistrital.edu.co/apioas/notificacion_mid/v1/ws",
+  NOTIFICACIONES_CRUD: "https://autenticacion.portaloas.udistrital.edu.co/apioas/notificaciones_crud/",
+
   TOKEN: {
     AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
     CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
